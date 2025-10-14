805788683a5b3d6c1f56862078fcf1587d463579
Jatin Gidh <134076892+Jatin0410@users.noreply.github.com>
2025-10-14 21:41:40 +0530

    Commit

--- JS/Assignments/Assignment2.js
+++ JS/Assignments/Assignment2.js
@@ -1,22 +1,22 @@
-// // Q1.Write a JavaScript program to get the first n elements of an array.[n can be any positive number]
+// Q1.Write a JavaScript program to get the first n elements of an array.[n can be any positive number]
 
-// let sizeA = parseInt(prompt("Enter the size of Array:"));
-// let arrayElement = [];
+let sizeA = parseInt(prompt("Enter the size of Array:"));
+let arrayElement = [];
 
-// for (let i = 0; i < sizeA; i++) {
-//   arrayElement.push(prompt(`Enter element ${i + 1}:`));
-// }
-// console.log(arrayElement);
+for (let i = 0; i < sizeA; i++) {
+  arrayElement.push(prompt(`Enter element ${i + 1}:`));
+}
+console.log(arrayElement);
 
-// // Now get first n elements
-// let n = parseInt(prompt("Enter how many first elements you want:"));
-// let firstN = arrayElement.slice(0, n);
-// console.log(`First ${n} elements:`, firstN);
+// Now get first n elements
+let n = parseInt(prompt("Enter how many first elements you want:"));
+let firstN = arrayElement.slice(0, n);
+console.log(`First ${n} elements:`, firstN);
 
-// // Q2 get last n elements
-// n = parseInt(prompt("Enter how many last elements you want:"));
-// let lastN = arrayElement.slice(-n);
-// console.log(`Last ${n} elements:`, lastN);
+// Q2 get last n elements
+n = parseInt(prompt("Enter how many last elements you want:"));
+let lastN = arrayElement.slice(-n);
+console.log(`Last ${n} elements:`, lastN);
 
 // Q3 .Write a JavaScript program to check whether a string is blank or not
 let sampleString = "";
@@ -37,3 +37,23 @@ if (char === char.toLowerCase() && char !== char.toUpperCase()) {
 
 // 05 
 console.log(sampleString2.trim());
+
+// Q6: 
+let sampleArray = [1, 2, 3, 3, 4, 5, 5, 7, 8, 9, 10];
+let findArray = parseInt(prompt("Enter the value to find in Array:"));
+
+let found = false; // flag variable
+
+for (let i = 0; i < sampleArray.length; i++) {
+  if (sampleArray[i] === findArray) {
+    found = true;
+    break; // stop searching once found
+  }
+}
+
+if (found) {
+  console.log("Element Found");
+} else {
+  console.log("Element Not Found");
+}
+
